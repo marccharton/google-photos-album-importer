aae8a881d9d2fccab05874ea485dd35c5b197503
marccharton <kramoogle@gmail.com>
2023-01-25 14:49:21 +0100

    chore(album creation): improve error handling

--- connect_to_google_photo.js
+++ connect_to_google_photo.js
@@ -5,14 +5,15 @@ import express from 'express';
 import open from 'open';
 import asyncForEach from 'async-await-foreach';
 import fs from "fs";
-import path from "path";
+// import path from "path";
+import {writeJsonFile} from 'write-json-file';
 
-let oauth2Client, photos, album;
-const app = express();
 dotenv.config();
-let photosData;
+const app = express();
 
-makeItDone("Processing albums to import...", () => photosData = JSON.parse(fs.readFileSync('./photos.json', 'utf8')));
+let oauth2Client, photos, photosData, errorReport = {};
+
+makeItDone("Processing albums to import...", () => photosData = JSON.parse(fs.readFileSync(process.env.PROCESSED_ALBUMS_FILE_NAME, 'utf8')));
 
 app.get('/', async function (req, res) {
   await open("http://localhost:8000/connect");
@@ -42,10 +43,7 @@ app.get('/connect', function(req, res) {
 app.get("/google/auth/redirect", async function (req, res) {
   
   const {tokens} = await oauth2Client.getToken(req.query.code)
-  //oauth2Client.setCredentials(tokens);
-  
-  console.log(tokens);
-  process.env.ACCESS_TOKEN = tokens.access_token 
+  process.env.ACCESS_TOKEN = tokens.access_token;
   
   res.redirect('/photos/menu');
 });
@@ -54,9 +52,7 @@ app.get('/photos/menu', function (req, res) {
   res.send(`C'est tout bon tu es connecté!! <br/> Tu peux tester avec les fonctionnalités suivantes : 
       <ul> 
         <li><a href="/photos/list" target="_blank">Liste des albums</a> </li> 
-        <li><a href="/photos/create" target="_blank">Créer un album </a> </li> 
         <li><a href="/photos/upload/album" target="_blank">Uploader un album </a> </li> 
-        <li><a href="/photos/upload/folder" target="_blank">Uploader un dossier </a> </li> 
       </ul>`);
 });
 
@@ -88,21 +84,6 @@ app.get('/photos/list', async function(req, res) {
   res.send(view);
 });
 
-app.get('/photos/create', async function(req, res) {
-  if (!process.env.ACCESS_TOKEN) {
-    res.redirect('/connect');
-    return;
-  }
-  else {
-    photos = new Photos(process.env.ACCESS_TOKEN);
-  }
-
-  const response = await photos.albums.create('Mon super album');
-  album = response;
-  console.log(response);
-  res.redirect(response.productUrl);
-});
-
 app.get('/photos/upload/album', async function(req, res) {
   if (!process.env.ACCESS_TOKEN) {
     res.redirect('/connect');
@@ -119,19 +100,19 @@ app.get('/photos/upload/album', async function(req, res) {
       console.log(`Uploading album "${albumName}" ...`);
   
       await asyncForEach(files, async file => {
-        await uploadingFile( 
-          album.id,
-          file.fileName,
-          file.filePath,
-          file.description,
-          100000,
-        );
+          await uploadingFile( 
+            album,
+            file.fileName,
+            file.filePath,
+            file.description,
+            100000,
+          );
       }).then(_ => {
-        console.log(`☑️  The album "${albumName}" imported successfully `);
+        console.log(`☑️ The album "${albumName}" imported ${errorReport[albumName] !== undefined
+                                                            ? 'with errors (check error report file)'
+                                                            : 'successfully'}`);
       }).catch(err => {
-        // TODO : loger l'erreur et passer à la suite plutôt que de tout arrêter.
-        console.log({albumName, err});
-        throw err;
+        console.log(err);
       })
     }
   }
@@ -140,90 +121,25 @@ app.get('/photos/upload/album', async function(req, res) {
     return ;
   }
   
+  if (!isEmpty(errorReport)) {
+    await writeJsonFile("error_report.json", errorReport);
+  }
+  
+  console.log("Every albums had been processed successfully. Check error report for more details.");
   res.send("C'est tout bon");
 });
-async function uploadingFile(albumId, fileName, filePath, description, requestDelay) {  
+async function uploadingFile(album, fileName, filePath, description, requestDelay) {  
   try {
     process.stdout.write(`\--> Uploading file "${fileName}" ...`);
 
-    const response = await photos.mediaItems.upload(albumId, fileName, filePath, description, requestDelay);
+    const response = await photos.mediaItems.upload(album.id, fileName, filePath, description, requestDelay);
     console.log(' Done ! 🎉');
 
     return response;
   }
   catch(err) {
-    console.log(err);
-    throw err;
-  }
-}
-
-app.get('/photos/upload/folder', async function(req, res) {
-  if (!process.env.ACCESS_TOKEN) {
-    res.redirect('/connect');
-    return;
-  }
-  else {
-    photos = new Photos(process.env.ACCESS_TOKEN);
-  }
-
-  if (!process.env.ALBUM_ID && (album === undefined || album === null)) {
-    res.send("<p>Navré mais l'album n'existe pas, t'es sûr que tu l'as créé avant ?!</p>");
-    return;
-  }
-
-  const files = [
-    { name: 'barbichette marc.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'GEDC1220.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080026.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080027.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080028.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080029.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080031.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080035.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080037.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080038.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080046.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080053.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080058.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080059.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080061.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080062.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080063.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080064.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080066.JPG', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080068.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080071.MOV', description: '2011.04.27 Champote avec ugo' },
-    { name: 'P1080073.JPG', description: '2011.04.27 Champote avec ugo' },
-  ];
-
-  const albumId = album 
-                  ? album.id
-                  : process.env.ALBUM_ID;
-
-  await uploadingFolder(
-    res, 
-    albumId,
-    files,
-    directoryPath = "D:\\Photos\\2011\\2011.04.27 Champote avec ugo\\P1080071.MOV",
-    requestDelay = 100000,
-  );
-});
-async function uploadingFolder(res, albumId, files, directoryPath, requestDelay) {  
-  try {
-    process.stdout.write(`Uploading [D] ${directoryPath}" ...`);
-
-    const response = await photos.mediaItems.upload(albumId, files, directoryPath, requestDelay);
-    console.log(' Done !');
-
-    console.log('======== RESPONSE ========');
-    console.log(response);
-
-    res.send({response});
-  }
-  catch(err) {
-    res.status(500).send({ error: err })
-    // res.send("Navré mais il y a eu un souci");
-    console.log(err);
+    log(album.title, err);
+    console.log(" Failed 😭");
   }
 }
 
@@ -246,4 +162,15 @@ function makeItDone(message, cb) {
   process.stdout.write(message);
   cb();
   console.log(" Done !");
+}
+
+function log(albumName, err) {
+  if (errorReport[albumName] === undefined) {
+    errorReport[albumName] = [];
+  }
+  errorReport[albumName].push(err);
+}
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
 }
